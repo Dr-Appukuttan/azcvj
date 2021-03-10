fb148d0b0c6236363912f9ebeb3033ba9226eaf8
Aᴘᴘᴜᴋᴜᴛᴛᴀɴ <73917119+Dr-Appukuttan@users.noreply.github.com>
2021-03-10 20:57:25 +0530

    Add files via upload

---
+++ script/scripts.js
@@ -0,0 +1,8 @@
+function telegram() {window.open("https://t.me/Abin 666");}
+function reddit() {window.open("https://www.reddit.com/u/google");}
+function instagram() {window.open("https://www.instagram.com/abin_karukappallil");}
+function github() {window.open("https://www.github.com/Dr-Appukuttan");}
+function gitlab() {window.open("https://www.gitlab.com/Dr-Appukuttan");}
+function discord() {window.open("https://discord.com/users/Aᴘᴘᴜᴋᴜᴛᴛᴀɴ#9185 ");}
+function msfauto() {window.open("https://www.github.com/Dr-Appukuttan/Discord-Music-Bot-With-Filters");}
+
